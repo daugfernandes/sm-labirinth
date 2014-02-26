d6fc2f99feb97e911fa0878a49a6a3a739292596
azserrata@gmail.com <azserrata@gmail.com@c058badc-48e7-dc88-b670-16dc839d5f23>
2014-02-26 13:41:57 +0000

    Referee class.

---
+++ sm-labirinth/src/sm/labirinth/Referee.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sm.labirinth;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ * @author david.paiva.fernandes@gmail.com
+ * @author joaorodr84@gmail.com
+ */
+public class Referee extends jade.core.Agent {
+
+  private static final long serialVersionUID = 1L;
+
+  private Labirinth _labirinth;
+  private List<Agent> _agents;
+
+  public Referee(String labirinthFilename) throws IOException {
+    this._labirinth = new Labirinth(labirinthFilename);
+    this._agents = new ArrayList<>();
+  }
+
+  public List<Agent> getAgents() {
+    return Collections.unmodifiableList(_agents);
+  }
+  
+}
