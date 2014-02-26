cf7e45e6f35189b3d8c77bcda87f5e0aedf11642
azserrata@gmail.com <azserrata@gmail.com@c058badc-48e7-dc88-b670-16dc839d5f23>
2014-02-26 10:01:19 +0000

    Starting the reading of labirinth definition text files. New public method getExit for Labirinth class.

--- trunk/sm-labirinth/src/sm/labirinth/Labirinth.java
+++ trunk/sm-labirinth/src/sm/labirinth/Labirinth.java
@@ -5,6 +5,11 @@
  */
 package sm.labirinth;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author david.paiva.fernandes@gmail.com
@@ -12,16 +17,57 @@ package sm.labirinth;
  */
 public class Labirinth {
 
-    Cell[][] _cells;
-    Cell _exit;
+  private Cell[][] _cells;
+  private Cell _exit;
 
-    public Labirinth() {
-        // by default
-        this(10, 10, new Cell(10, 10));
-    }
+  public Labirinth() {
+    // by default
+    this(10, 10, new Cell(10, 10));
+  }
 
-    public Labirinth(int width, int height, Cell exit) {
-        _cells = new Cell[width][height];
-        _exit = exit;
+  public Labirinth(String labirinthFileName) throws IOException {
+
+    String[] labirinthString = readTextFile(labirinthFileName);
+    int exitX = -1, exitY = -1;
+
+    // find width and height
+    int width = 0, height = 0;
+    for (String row : labirinthString) {
+      height++;
+      width = Math.max(width, row.length());
+      exitX = row.indexOf("*");
+      if (exitX >= 0) {
+        exitY = height - 1;
+      }
     }
+    _cells = new Cell[width][height];
+    _exit = new Cell(exitX, exitY);
+  }
+
+  public Labirinth(int width, int height, Cell exit) {
+    _cells = new Cell[width][height];
+    _exit = exit;
+  }
+
+  private String[] readTextFile(String fileName) throws FileNotFoundException, IOException {
+
+    StringBuilder sb = new StringBuilder();
+    BufferedReader br = new BufferedReader(new FileReader("file.txt"));
+    try {
+      String line = br.readLine();
+
+      while (line != null) {
+        sb.append(line);
+        sb.append(System.lineSeparator());
+        line = br.readLine();
+      }
+    } finally {
+      br.close();
+    }
+    return sb.toString().split(System.lineSeparator());
+  }
+  
+  public Cell getExit() {
+    return _exit;
+  }
 }
