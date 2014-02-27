6199e6095998822f945eaee51f75db9b1bfb91f7
azserrata@gmail.com <azserrata@gmail.com@c058badc-48e7-dc88-b670-16dc839d5f23>
2014-02-27 00:05:12 +0000

    New test classes after jade refactoring.

---
+++ trunk/sm-labirinth/test/sm/labirinth/LabirinthSuite.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sm.labirinth;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author David Fernandes <davidfernandes@acm.org>
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({sm.labirinth.labirinths.LabirinthsSuite.class})
+public class LabirinthSuite {
+
+  @BeforeClass
+  public static void setUpClass() throws Exception {
+  }
+
+  @AfterClass
+  public static void tearDownClass() throws Exception {
+  }
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+  
+}
